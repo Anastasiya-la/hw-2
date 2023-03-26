80b4c3a44a7aaab6d4b01586c7e79dbf19869412
Anastasiya Rabetskaya <nastia1362@gmail.com>
2023-03-26 21:14:45 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,7 +7,7 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            return action.payload === 'up' ? [...state.sort((a, b) => a.name > b.name ? 1 : -1)] : [...state.sort((a, b) => a.name < b.name ? 1 : -1)]
+            return action.payload === 'up' ? [...state].sort((a, b) => a.name > b.name ? 1 : -1) : [...state].sort((a, b) => a.name < b.name ? 1 : -1)
 
         }
 
